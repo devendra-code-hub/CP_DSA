eec1f421a74e14787a9880a429874ec1f1121402
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2025-06-28 11:21:46 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (79.13%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,17 +4,21 @@ public:
        int n=nums.size();
        vector<int>ans;
        k=k%n;
-       for(int i=n-k; i<n; ++i){
-        ans.push_back(nums[i]);
-       }  
 
-       for(int i=0; i<n-k; ++i){
-        ans.push_back(nums[i]);
-       }
+       reverse(nums.begin(), nums.end());
+       reverse(nums.begin(), nums.begin() + k );
+       reverse(nums.begin() + k, nums.end());
+    //    for(int i=n-k; i<n; ++i){
+    //     ans.push_back(nums[i]);
+    //    }  
 
-       for(int i=0; i<n; ++i){
-        nums[i] = ans[i];
-       }
+    //    for(int i=0; i<n-k; ++i){
+    //     ans.push_back(nums[i]);
+    //    }
+
+    //    for(int i=0; i<n; ++i){
+    //     nums[i] = ans[i];
+    //    }
         
     }
 };
