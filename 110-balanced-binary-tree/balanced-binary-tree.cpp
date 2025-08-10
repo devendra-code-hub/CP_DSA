0cb877cb5b72b6b55a914010f1c145e1c94d0ddb
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2025-08-10 11:07:31 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (83.68%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,21 +11,32 @@
  */
 class Solution {
     private:
-    int height(TreeNode* root){
-        if(root == NULL) return 0;
+    private:
+     pair<bool,int> isBalancedFast(TreeNode* root){
+         if(root == NULL) {
+             pair<bool,int> p = make_pair(true,0);
+             return p;
+         }
         
-        return max(height(root->left), height(root->right))+1;
-    }
+        pair<bool,int> left = isBalancedFast(root->left);
+         pair<bool,int> right = isBalancedFast(root->right);
+        
+        bool leftAns =  left.first;
+        bool rightAns = right.first;
+        bool diff = abs(left.second - right.second)<=1;
+        
+         pair<bool,int>ans;
+         ans.second = max(left.second, right.second) +1;
+         if(leftAns && rightAns && diff){
+             ans.first = true;
+         }else{
+             ans.first = false;
+         } 
+         
+        return ans;
+     }
 public:
     bool isBalanced(TreeNode* root) {
-        if(root == NULL) return true;
-        
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
-        bool diff = abs(height(root->left) - height(root->right))<=1;
-        
-        if(left && right && diff) return true;
-        
-        return false;
+         return isBalancedFast(root).first;
     }
 };
