b4439684cfb92a958c94c768916a2d1b5b6ec0d9
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2025-09-02 23:21:59 +0530

    Time: 1 ms (2.58%) | Memory: 13.5 MB (7.57%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -3,9 +3,21 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
+        
+        if( ( matrix[0][0] > target) ) {
+           return false;
+        }
+        int check_row =0;
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == target) return true;
+             if(matrix[i][0] == target ) return true;
+             else if(matrix[i][0] > target){ check_row =i-1;break;}
+        }
+        for(int i=0; i<m; i++){
+            if(matrix[check_row][i] == target) return true;
+        }
+        if(check_row == 0){
+            for(int i=0; i<m; i++){
+                if(matrix[n-1][i] == target) return true;
             }
         }
 
