9bbc5a843803be9c5a34854dbae3bae46b14ad5d
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2025-10-09 19:54:17 +0530

    Time: 0 ms (100.00%) | Memory: 72.7 MB (47.99%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         int  size = bookings.size();
-        vector<int>ans(n);
+        vector<int>ans(n+1,0);
         for(int i=0; i<size; i++){
-            for(int j=bookings[i][0] ; j<= bookings[i][1]; j++){
-                ans[j-1] += bookings[i][2]; 
-            }
+          //  for(int j=bookings[i][0] ; j<= bookings[i][1]; j++){
+            int p = bookings[i][0], q=bookings[i][1];
+                ans[p-1] += bookings[i][2]; 
+                ans[q] -= bookings[i][2]; 
+           // }
+        }
+        vector<int>prefix(n,0);
+        prefix[0] = ans[0];
+        for(int i=1; i<n; i++){
+            prefix[i] = prefix[i-1] + ans[i];
         }
 
-        return ans;
+        return prefix;
     }
 };
