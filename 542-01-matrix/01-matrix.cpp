50be1439a06375021005643cea6d3fb1bde2bf85
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-04-11 23:21:00 +0530

    Time: 8 ms (90.32%) | Memory: 38.9 MB (20.33%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size(),m=mat[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>>dist(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 0){
+                    q.push({{i,j},0});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            dist[row][col] = steps;
+            q.pop();
+            int delrow[4]={-1,0,1,0};
+            int delcol[4]={0,1,0,-1};
+            for(int i=0; i<4; i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol] ==0){
+                    q.push({{nrow,ncol},steps+1});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+        return dist;
+    }
+};
