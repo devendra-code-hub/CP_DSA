5d3f2243cad7030f9ab3e3e316e8acb4b80f0909
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-06-21 14:45:03 +0530

    Time: 7 ms (86.94%) | Memory: 9.3 MB (80.19%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size(),start=0,mxlen=0;
-        for(int i=0; i<n; ++i){
+        int n=s.size();
+        int len=0,stidx=-1;
+        for(int i=0; i<n; i++){
             for(int j=0; j<=1; j++){
-                int low = i;
-                int high = i+j;
-                while(low>=0 && high<n && s[low] == s[high]){
-                int currlen = high-low+1;
-                if(currlen>mxlen){
-                    start=low;
-                    mxlen=currlen;
-                }
-                low--;
-                high++;
+                int l=i;
+                int r=i+j;
+                while(l>=0 && r<n && s[l]==s[r]){
+                    if(len<r-l+1){
+                        len=r-l+1;
+                        stidx=l;
+                    }
+                    l--;
+                    r++;
                 }
             }
         }
-
-        return s.substr(start,mxlen);
+        return s.substr(stidx,len);
     }
 };
