cd399518605cbe236a28a88e5f3a72efca5d5a90
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-07-15 23:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (46.67%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+int gcd_c(int a, int b){
+    if(b==0) return a;
+    return gcd_c(b,a%b);
+}
+    int gcdOfOddEvenSums(int n) {
+        int so=0,se=0,cnt=0;
+        for(int i=1; i<=2*n; i++){
+            if(i&1)so+=i;
+            else se+=i;
+        }
+        return gcd_c(so,se);
+    }
+};
