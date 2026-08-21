275258a725943e8da74bebce7d032719b640d1fe
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-08-21 17:57:26 +0530

    Time: 26 ms (25.11%) | Memory: 17.5 MB (24.15%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
+  
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue; // skip duplicates
-
-            for (int j = i+1; j < n; j++) {
-                if (j > i+1 && nums[j] == nums[j-1]) continue; // skip duplicates
-
-                long long newTarget = (long long)target - nums[i] - nums[j];
-                int l = j+1, r = n-1;
-
-                while (l < r) {
-                    long long sum = nums[l] + nums[r];
-                    if (sum == newTarget) {
-                        ans.push_back({nums[i], nums[j], nums[l], nums[r]});
-
-                        // skip duplicates
-                        while (l < r && nums[l] == nums[l+1]) l++;
-                        while (l < r && nums[r] == nums[r-1]) r--;
-
-                        l++, r--;
-                    } else if (sum < newTarget) {
-                        l++;
-                    } else {
-                        r--;
+          int n=nums.size();
+          vector<vector<int>>ans;
+          sort(nums.begin(), nums.end());
+          for(int i=0; i<n; i++){
+            if(i>0 && nums[i-1]==nums[i]) continue;
+            for(int j=i+1; j<n; j++){
+                if(j>i+1 && nums[j-1]==nums[j]) continue;
+                 int left=j+1, right=n-1;
+                 while(left <right){
+                    long long sum= (long long) nums[i]+nums[j]+nums[left]+nums[right];
+                    
+                    if(sum == target){
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                         while(left<right && nums[left] == nums[left+1]) left++;
+                    while(left<right && nums[right-1] == nums[right]) right--;
+                    left++;
+                    right--;
                     }
-                }
+                  else  if(sum<target)left++;
+                    else right--;
+                 }
             }
-        }
+          }
 
-        return ans;
+          return  ans;
     }
 };
