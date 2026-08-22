9e1d673d9ca6df934f886ca8537d9d425370018e
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-08-22 21:39:06 +0530

    Time: 3 ms (36.27%) | Memory: 21.2 MB (9.23%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,41 +1,37 @@
 class Solution {
-private:
-    // dfs traversal function
-    void dfs(int node, vector<vector<int>>& adjLs, vector<int>& vis) {
-        // mark the node as visited
-        vis[node] = 1;
-        for (auto it : adjLs[node]) {
-            if (!vis[it]) {
-                dfs(it, adjLs, vis);
-            }
+public:
+void dfs(int node, vector<int>&vis, vector<vector<int>>&adj){
+    vis[node]=1;
+    for(auto it : adj[node]){
+        if(!vis[it]){
+            dfs(it, vis, adj);
         }
     }
 
-public:
-    int findCircleNum(vector<vector<int>>& adj) {
-        int V = adj.size(); // Use adj.size() to get the number of rows (vertices)
-        vector<vector<int>> adjLs(V);
-
-        // to change adjacency matrix to list
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                // self nodes are not considered
-                if (adj[i][j] == 1 && i != j) {
-                    adjLs[i].push_back(j);
+}
+    int findCircleNum(vector<vector<int>>& isConnected) {
+         
+        int n=isConnected.size();
+        vector<vector<int>>adj(n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(isConnected[i][j]) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
-        
-        vector<int> vis(V, 0); // Correctly initializes a vector of size V with all zeros
-        int cnt = 0;
-        for (int i = 0; i < V; i++) {
-            // if the node is not visited
-            if (!vis[i]) {
-                // counter to count the number of provinces
+        vector<int>vis(n,0);
+        int cnt=0;
+
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                dfs(i,vis, adj);
                 cnt++;
-                dfs(i, adjLs, vis);
             }
         }
+
         return cnt;
+
     }
 };
