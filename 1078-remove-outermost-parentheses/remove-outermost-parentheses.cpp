d07a882a4e6a5cfe1c21ee83f8f9b8243954dbd6
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-08-22 18:34:45 +0530

    Time: 1 ms (24.08%) | Memory: 9.9 MB (10.73%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string res;
-        int count=0;
-        for(char c: s){
-            if(c=='(' && count++>0){
-                res+=c;
-            }
-            if(c==')' && count-->1){
-                res+=c;
-            }
-
+        string ans="";
+        int n=s.size();
+        vector<int>l(n,0),r(n,0);
+        int cnt=0;
+        for(int i=0; i<n; i++){
+            if(s[i] =='('){
+                cnt++;
+                l[i]=cnt;
+           }else{
+            cnt--;
+            l[i]=cnt;
+           }
         }
-        return res;
-        
+        for(int i=n-1; i>=0; i--){
+            if(s[i] =='('){
+                cnt--;
+                r[i]=cnt;
+           }else{
+            cnt++;
+            r[i]=cnt;
+           }
+        }
+         
+        for(int i=0; i<n; i++){
+            if(l[i] == 0 || r[i] ==0) continue;
+            ans+=s[i];
+        }
+
+        return ans;
+
     }
 };
