2a069fd722d4500c89ff8d770a40641a4bbc73cb
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-08-23 17:30:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (24.78%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        int cnt=0,ans=0;
-        for(auto c : s){
-            if(c=='(') {cnt++;ans=max(ans,cnt);}
-            else if(c==')') cnt--;
-        }
-
-        return ans;
-        
+        int ans=0, cnt=0;
+        int n=s.size();
+         for(auto c: s){
+            if(c=='('){
+                cnt++;
+                ans=max(ans,cnt);
+            }else if( c==')')cnt--;
+         }
+         return ans;
     }
 };
