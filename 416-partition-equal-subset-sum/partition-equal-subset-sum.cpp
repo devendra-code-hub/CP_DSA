b75d1bcbf7044672bde7adc065bfefdfcfb0c4ab
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-08-30 13:43:54 +0530

    Time: 327 ms (5.01%) | Memory: 262.4 MB (5.05%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    private :
-       bool f(int idx, int s, int sum, int n, vector<int>&nums, vector<vector<int>>&v){
-        if(s==sum-s) return true;
-        if(idx>=n || s>sum-s) return false;
-        if(v[idx][s] !=-1) return v[idx][s];
-        bool l= f(idx+1, s+nums[idx],sum, n,nums,v);
-        bool r= f(idx+1, s,sum, n,nums,v);
-
-        return v[idx][s]=l||r;
-       }
 public:
+int f(int i, int sum, int n, int ts, vector<int>&nums, vector<vector<int>>&dp){
+    if(sum == (ts-sum)) return 1;
+    if(sum>(ts-sum) || i>=n) return 0;
+    if(dp[i][sum] != -1) return dp[i][sum];
+    int t= f(i+1, sum+nums[i], n, ts, nums, dp);
+    int nt= f(i+1, sum, n, ts, nums, dp);
+
+    return dp[i][sum]= t || nt;
+}
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        vector<vector<int>>v(n,vector<int>(sum,-1));
+        int ts=accumulate(nums.begin(), nums.end(),0);
+        vector<vector<int>>dp(n, vector<int>(ts,-1));
 
-        return f(0,0,sum,n,nums,v);
-
+        return f(0,0,n,ts,nums,dp);
     }
 };
