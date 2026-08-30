fa630886164b592b5f9115435a0d38257b4d4399
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-08-30 09:35:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (39.23%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -9,7 +9,14 @@ int f(int sum, int n, vector<int>&dp){
     return dp[sum]=s1+s2;
 }
     int climbStairs(int n) {
-        vector<int>dp(n,-1);
-        return f(0,n,dp);
+        // vector<int>dp(n,-1);
+        // return f(0,n,dp);
+        vector<int>dp(n+1,0);
+        dp[n]=1;
+        for(int i=n-1; i>=0; i--){
+            if(i+2<=n)dp[i]+=dp[i+2];
+            dp[i]+=dp[i+1];
+        }
+        return dp[0];
     }
 };
