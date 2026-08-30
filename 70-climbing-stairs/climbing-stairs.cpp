9ffd9ef7d4a2b24513ee134af979e1d0ccb69281
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-08-30 09:23:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (54.41%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+int f(int sum, int n, vector<int>&dp){
+    if(sum==n) return 1;
+    if(sum>n) return 0;
+    if(dp[sum] != -1) return dp[sum];
+    int s1=f(sum+1, n, dp);
+    int s2=f(sum+2, n, dp);
+    return dp[sum]=s1+s2;
+}
+    int climbStairs(int n) {
+        vector<int>dp(n,-1);
+        return f(0,n,dp);
+    }
+};
