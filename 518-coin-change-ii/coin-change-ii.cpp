1d470b97451a80f2581548ec6dc3c46b39a66d8a
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-08-31 10:13:18 +0530

    Time: 59 ms (8.12%) | Memory: 36.9 MB (27.17%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-int f(int idx, int sum, int n, int amount, vector<int>&coins, vector<vector<int>>&dp){
-    if(sum== amount) return 1;
-    if(sum> amount || idx>=n) return 0;
-    if(dp[idx][sum] !=-1 ) return dp[idx][sum];
-    int t=f(idx, sum+coins[idx], n, amount, coins, dp);
-    int nt=f(idx+1, sum, n, amount, coins, dp);
+int f(int i, int sum, int n, int amt, vector<int>&coins, vector<vector<int>>&dp){
+    if(sum==amt) return 1;
+    if(sum>amt || i>=n) return 0;
+    if(dp[i][sum] != -1) return dp[i][sum];
+    int t=f(i, sum+coins[i], n, amt, coins, dp);
+    int nt=f(i+1, sum, n, amt, coins, dp);
 
-    return dp[idx][sum]=t+nt;
+    return dp[i][sum]=t+nt;
 }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        if(amount==0) return 1;
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        f(0,0,n,amount,coins,dp);
-        return dp[0][0]==-1 ? 0: dp[0][0];
+        vector<vector<int>>dp(n, vector<int>(amount,-1));
+        return f(0,0,n, amount, coins, dp);
     }
 };
