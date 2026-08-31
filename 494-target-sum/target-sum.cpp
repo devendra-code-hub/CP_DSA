956c57e8a150d6db703b9af9778ed4c8ed2e0e6b
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-08-31 09:45:11 +0530

    Time: 19 ms (43.87%) | Memory: 41.2 MB (16.53%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-using ll =long long;
-int f(int idx, int sum, int n, int target, vector<int>&nums, vector<vector<int>>&dp){
-   if(idx == n){
-    if(sum == target) return 1;
-    return 0;
-    }
-    // if(idx>=n) return 0;
-    if(dp[idx][sum +1000] != -1) return dp[idx][sum+1000];
-    int a=f(idx+1, sum+nums[idx], n, target, nums, dp);
-    int s=f(idx+1, sum-nums[idx], n, target, nums, dp);
+using ll=long long;
+int f(int i, int sum, int n, int target, vector<int>&nums, vector<vector<int>>&dp){
+   if(i==n){
+         if(sum == (target+1e3)) return 1;
+         return 0;
+   }
+    
+    if(i>=n) return 0;
+    if(dp[i][sum] != -1) return dp[i][sum];
+    int add= f(i+1, sum+nums[i], n, target, nums, dp);
+    int sub= f(i+1, sum-nums[i], n, target, nums, dp);
 
-    return dp[idx][sum+1000]=a+s;
+    return dp[i][sum]=add+sub;
 }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         vector<vector<int>>dp(n, vector<int>(2001,-1));
-        f(0,0,n,target,nums,dp);
-        return dp[0][0+1000];
+        return f(0, 1e3,n,target, nums, dp);
     }
 };
