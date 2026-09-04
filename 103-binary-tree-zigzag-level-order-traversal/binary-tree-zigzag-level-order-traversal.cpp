232f2cf517d0b415efe5c380ba78d5a4a086b1c4
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-09-04 20:26:24 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (49.05%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -12,35 +12,30 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-         
+        vector<vector<int>>res;
+        if(root==NULL) return res;
         queue<TreeNode*>q;
         q.push(root);
-        vector<vector<int>>result;
-        if(root == NULL){
-            return result;
-        }
-        
-        bool leftToRight = true;
-        
+        bool LtoR=true;
         while(!q.empty()){
-            int size = q.size();
+            int size=q.size();
             vector<int>ans(size);
-            
             for(int i=0; i<size; i++){
-                TreeNode* frontNode = q.front();
+                auto node=q.front();
                 q.pop();
-                int index = leftToRight ? i : size-i-1;
-                ans[index] = frontNode->val;
-                
-                if(frontNode->left) q.push(frontNode->left);
-                if(frontNode->right) q.push(frontNode->right);
+                int idx=LtoR?i:size-i-1;
+                ans[idx]=node->val;
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
             }
-            
-            leftToRight = !leftToRight;
-            
-            result.push_back(ans);
+            res.push_back(ans);
+            LtoR=!LtoR;
         }
-        
-        return result;
+
+        return res;
     }
 };
