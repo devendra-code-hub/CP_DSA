f781e9a5429b063303999ba09408c8d154c1c4a4
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-09-14 19:12:53 +0530

    Time: 51 ms (46.30%) | Memory: 19.3 MB (47.59%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n=s.size();
+        if(s.size()==0) return 0;
          unordered_map<char,int>mp;
-        int ans=0,l=0;
-        for(auto r=0; r<n;  r++){
+         int ans=INT_MIN,l=0;
+         for(int r=0; r<s.size(); r++){
             mp[s[r]]++;
             while(l<=r && mp[s[r]]>1){
                 mp[s[l]]--;
                 l++;
             }
-            ans=max(ans, r-l+1);
 
-        }
-        return ans;
+            ans=max(ans, r-l+1);
+         }
+
+         return ans;
     }
 };
