e3bfc637eadb55ddbd5cb2cbe752163cbe96fab6
DEVENDRA KUMAR MAHTO <devendra251india@gmail.com>
2026-09-17 17:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (78.44%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-     int n=nums.size();
-     int farthest=0,step=0,curr_idx=0;
-     for(int i=0; i<n-1; ++i){
-         farthest=max(farthest,i+nums[i]);
-        if(i==curr_idx){
-            step++;
-            curr_idx=farthest;
+        int cnt=0, mx=0,curr_end=0;
+        if(nums.size()==1)return 0;
+        for(int i=0; i<nums.size()-1; i++){ 
+             
+            mx=max(mx, i+nums[i]);
+            if(i==curr_end){
+            cnt++;
+            curr_end=mx;
+
+            if(curr_end>=nums.size()-1) break;
+            }
+             
+            
         }
-       
-     }
-     return step;
+
+        return cnt;
     }
 };
